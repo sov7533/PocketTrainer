14b38d5eba98bcff3e718cea41fb4a5cd4b8741b
sov7533 <sov-96@yandex.ru>
2018-11-28 15:11:16 +0400

    project skeleton

---
+++ PocketTrainer/src/test/java/com/pocket/trainer/PocketTrainer/PocketTrainerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pocket.trainer.PocketTrainer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PocketTrainerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
